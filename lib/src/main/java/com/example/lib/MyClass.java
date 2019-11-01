51127fbe271af84b32b0b5e7e9e0a2964966db95
TUF GAMING <oath0123@gmail.com>
2019-11-01 18:17:15 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -0,0 +1,12 @@
+package com.example.lib;
+
+public class MyClass {
+    public static void main(String args[]) {
+
+        java.util.Scanner scanner = new
+                java.util.Scanner(System.in);
+        System.out.print("input birthday:");
+        int input1 = scanner.nextInt();
+        System.out.print("birthday:" + input1);
+    }
+}
